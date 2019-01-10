4dc94f49806e5de883a94602b5846e82b9c3dc69
Rogerio Moura <rosestolato.rogerio@gmail.com>
2019-01-10 14:10:06 -0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $(".wrap").click(function(){
+      $(".top").toggleClass("addedtop");
+      $(".mid").toggleClass("addedmid");
+      $(".bot").toggleClass("addedbot");
+  $(".side").toggleClass("triggered");
+      $(".sidebtn").toggleClass("sideopacity");
+  
+  
+    });
+    
+  }); //final
